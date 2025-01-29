cfc76c234f1882fabbf63d2019b8a97ac00c3908
walid lhaila <walidlhaila00@gmail.com>
2025-01-29 22:42:20 +0100

    Feat: DATABASE CONNECTION

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import * as process from 'node:process';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306', 10),
+      username: 'root',
+      password: 'walid',
+      database: 'invest',
+      entities: [__dirname + '/../**/*.entity.{ts,js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
