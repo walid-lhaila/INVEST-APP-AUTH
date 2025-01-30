c3c966abfdba6a087d6c83758e199b37b2e4d05c
walid lhaila <walidlhaila00@gmail.com>
2025-01-30 22:08:50 +0100

    Feat: IMPLEMENT MICROSERVICES AUTH SERVICE WITH API GATEWAY SERVICE

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 dotenv.config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 3000,
+      },
+    },
+  );
+  await app.listen();
+  console.log('AUTHENTICATION SERVICE is running on: http://localhost:3000');
 }
 bootstrap();
